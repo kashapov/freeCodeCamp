944f97cffc5200f47041846fa24418179f48decd
Andrey Kashapov <kashapovandrey@gmail.com>
2019-03-12 09:27:37 +0300

    add build

---
+++ random-quote-machine/build/precache-manifest.7271464ca6cdfa8cd25b499babc91638.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "fca6b87e69c525701c5e",
+    "url": "/static/js/main.5e0d24fd.chunk.js"
+  },
+  {
+    "revision": "5c47a10d2b2b423cfb9d",
+    "url": "/static/js/2.7639dbd4.chunk.js"
+  },
+  {
+    "revision": "fca6b87e69c525701c5e",
+    "url": "/static/css/main.95115c6c.chunk.css"
+  },
+  {
+    "revision": "5a92f7ac41017791b2eb6f45ea1aec1c",
+    "url": "/index.html"
+  }
+];
