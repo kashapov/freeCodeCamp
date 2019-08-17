54c5c4018ee760de8767047dc25e16d49977fc08
Andrey Kashapov <kashapovandrey@gmail.com>
2019-08-17 10:22:31 +0300

    add app to docs & update readme

--- calculator/README.md
+++ calculator/README.md
@@ -5,7 +5,7 @@
 Created for FreeCodeCamp Front End Libraries Projects - Build a Drum Machine
 [Build a JavaScript Calculator](https://learn.freecodecamp.org/front-end-libraries/front-end-libraries-projects/build-a-javascript-calculator)
 
-[Link to app](https://github.com/kashapov/)
+[Link to app](https://kashapov.github.io/freeCodeCamp/calculator/)
 
 # Author
 Andrey Kashapov
---
+++ docs/calculator/precache-manifest.44f6f1af94313fdffe06a67b09a26fb2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e271e4841986462bbc06b5e5db7f7c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5257b3ea54acd75d2508",
+    "url": "/static/css/main.281fbc18.chunk.css"
+  },
+  {
+    "revision": "f7d2a363a9421ac50a58",
+    "url": "/static/js/2.c32d10aa.chunk.js"
+  },
+  {
+    "revision": "5257b3ea54acd75d2508",
+    "url": "/static/js/main.eebc7a78.chunk.js"
+  },
+  {
+    "revision": "b6d43c60bae782112689",
+    "url": "/static/js/runtime~main.48e4b13a.js"
+  }
+]);
