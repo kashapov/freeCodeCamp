53b4da479339558530a4ecf69a6d447fc36a134b
Andrey Kashapov <kashapovandrey@gmail.com>
2019-08-31 09:32:16 +0300

    add pomodoro-clock to /docs

---
+++ docs/pomodoro-clock/precache-manifest.1284aafb97503142d792e22c740a03ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a18dc0574460e03f348b667f76f237c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "719a0685f9568ea6e7dc",
+    "url": "/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "c05461d16b2c34a1c6ef",
+    "url": "/static/css/main.d934d7fe.chunk.css"
+  },
+  {
+    "revision": "719a0685f9568ea6e7dc",
+    "url": "/static/js/2.9bcadc90.chunk.js"
+  },
+  {
+    "revision": "c05461d16b2c34a1c6ef",
+    "url": "/static/js/main.719d082a.chunk.js"
+  },
+  {
+    "revision": "c3670e690929a3207560",
+    "url": "/static/js/runtime~main.e6c3e565.js"
+  }
+]);
